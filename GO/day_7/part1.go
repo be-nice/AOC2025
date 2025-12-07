9a8c9a4d801bcde9291b875f6f08f594ffcef3c1
be-nice <mobcarma@gmail.com>
2025-12-07 16:00:09 +0200

    Performance day 7 part 1, change from map to slice

--- GO/day_7/part1.go
+++ GO/day_7/part1.go
@@ -11,20 +11,32 @@ func Part1(f string) {
 	data := utils.ReadByteLines(path[f])
 	count := 0
 
-	beams := make(map[int]struct{})
-	beams[bytes.Index(data[0], []byte("S"))] = struct{}{}
+	beams := make([]bool, len(data[0]))
+	beams[bytes.Index(data[0], []byte("S"))] = true
 
 	for _, line := range data[1:] {
-		for i, el := range line {
-			if el == '^' {
-				if _, ok := beams[i]; ok {
-					delete(beams, i)
-					beams[i-1] = struct{}{}
-					beams[i+1] = struct{}{}
-					count++
+		next := make([]bool, len(beams))
+		for col, ok := range beams {
+			if !ok {
+				continue
+			}
+
+			if line[col] == '^' {
+				if col-1 >= 0 {
+					next[col-1] = true
 				}
+
+				if col+1 < len(line) {
+					next[col+1] = true
+				}
+
+				count++
+			} else {
+				next[col] = true
 			}
 		}
+
+		beams = next
 	}
 
 	fmt.Println(count)
